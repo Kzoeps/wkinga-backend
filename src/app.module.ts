f20fb6082618fdb7a2dadb86ffe60d1999761291
Kzoeps <kzoepa@gmail.com>
2020-12-01 00:21:51 +0600

    Database set up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
 })
 export class AppModule {}
