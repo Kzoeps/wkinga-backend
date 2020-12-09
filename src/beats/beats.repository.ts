7c901435313b1fec0d1a533f81a296ec776da567
Kzoeps <kzoepa@gmail.com>
2020-12-09 20:41:38 +0600

    feat(beats/): beats can now be updated
    To convert to a boolean for sold – used a very hacky way.

--- src/beats/beats.repository.ts
+++ src/beats/beats.repository.ts
@@ -7,29 +7,34 @@ import { UpdateBeatDto } from './dto/update-beat.dto';
 @Injectable()
 @EntityRepository(BeatsEntity)
 export class BeatsRepository extends Repository<BeatsEntity> {
-	async createBeat(createBeatDto: CreateBeatDto): Promise<BeatsEntity>{
-		const {beatName, beatProducer, audioURL, price, sold} = createBeatDto;
-		const beat = new BeatsEntity()
+	async createBeat(createBeatDto: CreateBeatDto): Promise<BeatsEntity> {
+		const { beatName, beatProducer, audioURL, price, sold } = createBeatDto;
+		const beat = new BeatsEntity();
 		beat.beatName = beatName;
 		beat.beatProducer = beatProducer;
 		beat.audioURL = audioURL;
 		beat.price = price;
 		beat.sold = sold;
-		await beat.save()
-		return beat
+		await beat.save();
+		return beat;
 	}
 
-	async updateBeat(id: number,updateBeatDto: UpdateBeatDto) {
-		// let beat = await this.findOne(id);
-		for (const property in updateBeatDto) {
-			if (updateBeatDto[property] !== 'undefined'){
-				console.log(property)
-			}
-		}
-	}
-
-	async getBeat(id: number): Promise<BeatsEntity>{
+	async updateBeat(id: number, updateBeatDto: UpdateBeatDto): Promise<BeatsEntity>{
 		const beat = await this.findOne(id);
-		return beat
+		for (const property in updateBeatDto) {
+			if (updateBeatDto[property] !== 'undefined') {
+				beat[property] = updateBeatDto[property];
+				if (property === 'sold'){
+					beat[property] = updateBeatDto[property].toString() === 'true';
+				}
+					}
+		}
+		await beat.save();
+		return beat;
+	}
+
+	async getBeat(id: number): Promise<BeatsEntity> {
+		const beat = await this.findOne(id);
+		return beat;
 	}
 }
