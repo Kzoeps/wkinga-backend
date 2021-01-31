3962c5cf22f420857a8aeed93ab470dfbe5b813b
JigmeT12 <jigmetashi02@gmail.com>
2021-02-01 00:29:26 +0400

    FEAT: repo initialized

---
+++ src/auth/admin.repository.ts
@@ -0,0 +1,7 @@
+import {EntityRepository, Repository} from 'typeorm';
+import {Admin} from './admin.entity';
+
+@EntityRepository(Admin)
+export class AdminRepository extends Repository<Admin> {
+    
+}
