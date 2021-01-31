ebce0591bd3fd6b7da00350afab64ba49554a7ff
JigmeT12 <jigmetashi02@gmail.com>
2021-02-01 01:04:48 +0400

    FEAT: validation for admin emil and password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,14 @@
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto{
+    @IsString()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(25)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {message: 'password must contain at least 1 upper case, lowercase and number or special letter'})
     password: string;
 }
